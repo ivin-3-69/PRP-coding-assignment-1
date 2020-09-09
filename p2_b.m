28bb4e92b1cae595a6a6b3c9acd1228ff0014ef2
Sivaprasad <unni007prasad@gmail.com>
2020-09-09 13:40:12 +0530

    change

---
+++ p2_b.m
@@ -0,0 +1,10 @@
+y=randn(1,100000);
+z=hist(y,100);
+z=z/100000;
+x=[0.01:0.01:1];
+avg = sum(x.*z);
+disp(avg);
+
+avg_vect = avg*ones(1,100);
+varience = sum(((x-avg_vect).*(x-avg_vect)).*z);
+disp(varience);
